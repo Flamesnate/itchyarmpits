521250ee21011f8cf6ce50e6d86c6823879a2a26
Nathan  Phabmixay <nathanphabmixay@gmail.com>
2023-04-06 22:14:17 +0000

    ichyarmpits

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<div class="backround">
+    <div class="layout">
+            <slot />
+    </div>
+</div>
+
+<style>
+    .backround {
+        position: absolute;
+        inset: 0;
+    
+        background-color:  #abacc2;
+    }
+
+    .layout {
+        width: 100%;
+        max-width: 900px;
+        margin: 0 auto;
+    }
+</style>
