cc4222b354cde361edb891e25dd9a31e8c3147cc
Nathan  Phabmixay <nathanphabmixay@gmail.com>
2023-04-14 15:14:39 +0000

    itchyarmpits

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,45 @@
+<script>
+    import Score from "../components/score.svelte";
+</script>
 <div class="backround">
     <div class="layout">
-            <slot />
-    </div>
-</div>
+        <div class="header">
+            <Score score={0} />
+            </div>
+            <div class="content">
+                <slot />
+                </div>
+                </div>
+            </div>
 
 <style>
     .backround {
+        padding: 32px;
+        display: grid;
         position: absolute;
         inset: 0;
-    
-        background-color:  #abacc2;
+        background-color: var(--color-purple)
     }
 
     .layout {
         width: 100%;
-        max-width: 900px;
+        max-width: 1024px;
         margin: 0 auto;
+
+        display: grid;
+        grid-template-rows: auto 1fr;
+        align-items: center;
+    }
+
+    .header,
+    .content {
+        background-color: white;
+        border: 4px solid black;
+        padding: 12px 24px;
+    }
+
+    .header {
+        display: flex;
+        justify-content: space between;
     }
 </style>
